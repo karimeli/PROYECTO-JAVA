5886a29bdc940dfc13c49c2ccaa20702794748b4
karimeli <karimcarlos32@icloud.com>
2024-02-21 23:11:36 -0600

    Avance3.5

--- js/app.js
+++ js/app.js
@@ -1,43 +1,45 @@
-const Ingreso = require('./Ingreso');
-const Egreso = require('./Egreso');
+const egresos = {
+    Renta: 4000,
+    Ropa: 800
+};
 
-
-const ingresos = [
-    new Ingreso('Salario', 20000),
-    new Ingreso('Venta auto', 50000)
-];
-
-const egresos = [
-    new Egreso('Renta', 4000),
-    new Egreso('Ropa', 800)
-];
-
+const ingresos = {
+    Salario: 20000,
+    'Venta auto': 50000
+};
 
 const totalIngresos = () => {
     let totalIngreso = 0;
-    for (const ingreso of ingresos) {
-        totalIngreso += ingreso.valor;
+    for (const key in ingresos) {
+        totalIngreso += ingresos[key];
     }
     return totalIngreso;
 };
 
 const totalEgresos = () => {
     let totalEgreso = 0;
-    for (const egreso of egresos) {
-        totalEgreso += egreso.valor;
+    for (const key in egresos) {
+        totalEgreso += egresos[key];
     }
     return totalEgreso;
 };
-
 
 const cargarCabecera = () => {
     const presupuesto = totalIngresos() - totalEgresos();
     const porcentajeEgreso = totalEgresos() / totalIngresos();
 
-    console.log(presupuesto);
-    console.log(porcentajeEgreso);
-    console.log(formatoMoneda(totalIngresos()));
-    console.log(formatoMoneda(totalEgresos()));
+    console.log('Presupuesto:', formatoMoneda(presupuesto));
+    console.log('Porcentaje de Egreso:', formatoPorcentaje(porcentajeEgreso));
+    console.log('Total Ingresos:', formatoMoneda(totalIngresos()));
+    console.log('Total Egresos:', formatoMoneda(totalEgresos()));
+};
+
+const formatoMoneda = (valor) => {
+    return valor.toLocaleString('es-MX', { style: 'currency', currency: 'MXN', minimumFractionDigits: 2 });
+};
+
+const formatoPorcentaje = (valor) => {
+    return valor.toLocaleString('es-MX', { style: 'percent', minimumFractionDigits: 2 });
 };
 
 cargarCabecera();
